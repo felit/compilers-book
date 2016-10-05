39c1bf4edd6f644a36f578e78928776d25ad8ee5
felit <felit123456>
2016-10-06 00:23:40 +0800

    添加异常

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,10 +3,12 @@ package main;
 import lexer.Lexer;
 import parser.Parser;
 
+import java.io.IOException;
+
 /**
  */
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         Lexer lex = new Lexer();
         Parser parse = new Parser(lex);
         parse.program();
